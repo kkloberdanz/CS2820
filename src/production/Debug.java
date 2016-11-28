87ffb1c4cb63056010b5f6df64d8504e440c7dfd
kkloberdanz <kyle.g.kloberdanz@uiowa.edu>
2016-11-28 17:27:01 -0600

    added debug levels

---
+++ src/production/Debug.java
@@ -0,0 +1,17 @@
+package production;
+
+public class Debug {
+	/*
+	private static final boolean verbose = false;
+	
+	public static boolean isVerbose() {
+		return verbose;
+	}
+	*/
+	
+	private static final int level = 5;
+	
+	public static int verboseLevel() {
+		return level;
+	}
+}
