52c300905c42e6493f011ee39a5c316ad88ad9e7
harrymj1007 <shuhao-liu@uiowa.edu>
2016-12-07 15:14:01 -0600

    Update RobotScheduler.java

--- src/production/RobotScheduler.java
+++ src/production/RobotScheduler.java
@@ -1,28 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package production;
+package CS2820;
 
 import java.awt.Point;
 
 /**
  *
- * @author lenovo
+ * @author Shuhao Liu
  */
 public interface RobotScheduler {
+	
+	/**
+     * 
+     * 
+     * @param Point a is the robot need to be move
+     * the robot will move one step at each time
+     * @return 
+     */
+	
+	void moveRobot(Robot a);
     /**
      * 
      * It deliever shelf A to the location B
      * @param Point A is the shelf
      * @param Point B is the destination that shelf should be moved to
+     * @return 
      */
-    //void moveShelf(Shelf A, Point p);
+    void moveShelf(Shelf A, Point p);
+    /**
+     * 
+     * It turns back shelf A to the location B
+     * @param Point A is the shelf
+     * @param Point P is the point A need to come back
+     * @param i is the robot i
+     * @return 
+     */
+    void returnShelf(Shelf A, Point p, int i);
     /**
      * find an available robot which is not used and in the best location with available charge
      */
-    boolean findAvailableRobot(Robot r);
+    
     /**
      * charge robot
      */
@@ -30,6 +45,7 @@ public interface RobotScheduler {
     /**
      * 
      */
+    int getAvailableRobotIndex(); 
     
 }
     
